c82d25363b517c079fd469a5952564c20cda6b0b
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2023-09-10 17:20:25 +0800

    CSS update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = "1691392069";
+const version = "1694337539";
 const note = "note";
 const other = "other";
 const base = location.origin + location.pathname.slice(0, -5); // remove last "sw.js"
