4743825909e243c86efa9cf78859fa0ac72d3234
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2024-02-26 14:59:25 +0800

    Use `location.href`

--- notes/note_js/qq_redpack.js
+++ notes/note_js/qq_redpack.js
@@ -6,6 +6,6 @@
         const qq = data.get("qq");
         const type = data.get("dest-type");
         const url = `https://h5.qianbao.qq.com/sendRedpack/index?_wv=67112960&_wvNt=0xFFFFFF&_wvSb=1&recv_uin=${qq}&recv_type=${type}`;
-        window.open(url);
+        location.href = url;
     });
 })();
