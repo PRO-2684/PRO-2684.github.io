a12a48d70309564e8fd1799a35f091c42a3f80bc
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2024-06-14 18:12:41 +0800

    Finish `rtl_lab`

--- notes/note_js/rtl_lab.js
+++ notes/note_js/rtl_lab.js
@@ -7,11 +7,17 @@
         static get lro() {
             return "\u202D";
         }
+        static #createElement(tag, classes = [], textContent = "") {
+            const element = document.createElement(tag);
+            element.classList.add(...classes);
+            element.textContent = textContent;
+            return element;
+        }
         constructor(options = {}) {
             this.mergeAdjacent = options.mergeAdjacent ?? true;
+            this.visualized = options.visualized ?? Visualizer.#createElement("div", ["rtl-visualized"]);
             this.text = "";
             this.cursor = 0;
-            this.visualized = document.createElement("div");
         }
         setText(text) {
             this.text = text;
@@ -19,101 +25,189 @@
             this.visualize();
         }
         insert(char) {
-            // ...
             this.text = this.text.slice(0, this.cursor) + char + this.text.slice(this.cursor);
             this.cursor++;
+            // TODO: More efficient implementation...
+            this.visualize();
         }
         backspace() {
-            // ...
             this.text = this.text.slice(0, this.cursor - 1) + this.text.slice(this.cursor);
             this.cursor--;
+            // TODO: More efficient implementation...
+            this.visualize();
         }
         delete() {
-            // ...
             this.text = this.text.slice(0, this.cursor) + this.text.slice(this.cursor + 1);
+            // TODO: More efficient implementation...
+            this.visualize();
         }
         move(delta) {
-            // ...
             this.cursor = Math.max(0, Math.min(this.text.length, this.cursor + delta));
+            // TODO: More efficient implementation...
+            this.visualize();
         }
         visualize() {
-            this.visualized.remove();
-            this.visualized = document.createElement("div");
+            this.visualized.innerHTML = "";
             let current = this.visualized;
             for (let i = 0; i < this.text.length; i++) {
                 const char = this.text[i];
                 const isCursor = i === this.cursor;
                 const isRLO = char === Visualizer.rlo;
                 const isLRO = char === Visualizer.lro;
-                if (isCursor && isRLO) {
-                    const span = document.createElement("span");
-                    span.textContent = "⬅️";
-                    span.classList.add("cursor");
-                    span.classList.add("rlo");
-                    const shouldMerge = this.mergeAdjacent && current.classList.contains("rlo");
-                    if (shouldMerge) {
-                        current.appendChild(span);
-                    } else {
-                        const div = document.createElement("div");
-                        div.classList.add("rlo");
-                        div.appendChild(span);
-                        current.appendChild(div);
-                        current = div;
-                    }
-                } else if (isCursor && isLRO) {
-                    const span = document.createElement("span");
-                    span.textContent = "➡️";
-                    span.classList.add("cursor");
-                    span.classList.add("lro");
-                    const shouldMerge = this.mergeAdjacent && current.classList.contains("lro");
-                    if (shouldMerge) {
-                        current.appendChild(span);
-                    } else {
-                        const div = document.createElement("div");
-                        div.classList.add("lro");
-                        div.appendChild(span);
-                        current.appendChild(div);
-                        current = div;
-                    }
+                const direction = isRLO ? "rlo" : "lro";
+                const arrow = isRLO ? "⬅️" : "➡️";
+
+                if (isRLO || isLRO) {
+                    current = this.#handleDirection(current, direction, arrow, isCursor);
                 } else if (isCursor) {
-                    const span = document.createElement("span");
-                    span.textContent = char;
-                    span.classList.add("cursor");
+                    const span = Visualizer.#createElement("span", ["cursor"], char);
                     current.appendChild(span);
-                } else if (isRLO) {
-                    const span = document.createElement("span");
-                    span.textContent = "⬅️";
-                    span.classList.add("rlo");
-                    const shouldMerge = this.mergeAdjacent && current.classList.contains("rlo");
-                    if (shouldMerge) {
-                        current.appendChild(span);
-                    } else {
-                        const div = document.createElement("div");
-                        div.classList.add("rlo");
-                        div.appendChild(span);
-                        current.appendChild(div);
-                        current = div;
-                    }
-                } else if (isLRO) {
-                    const span = document.createElement("span");
-                    span.textContent = "➡️";
-                    span.classList.add("cursor");
-                    span.classList.add("lro");
-                    const shouldMerge = this.mergeAdjacent && current.classList.contains("lro");
-                    if (shouldMerge) {
-                        current.appendChild(span);
-                    } else {
-                        const div = document.createElement("div");
-                        div.classList.add("lro");
-                        div.appendChild(span);
-                        current.appendChild(div);
-                        current = div;
-                    }
                 } else { // Normal character
                     current.appendChild(document.createTextNode(char));
                 }
             }
+            if (this.cursor === this.text.length) {
+                const span = Visualizer.#createElement("span", ["cursor"], " ");
+                current.appendChild(span);
+            }
+        }
+        #handleDirection(current, direction, arrow, isCursor = false) {
+            const span = Visualizer.#createElement("span", [direction], arrow);
+            if (isCursor) {
+                span.classList.add("cursor");
+            }
+            const shouldMerge = this.mergeAdjacent && current.classList.contains(direction);
+            current.appendChild(span);
+            if (!shouldMerge) {
+                const div = Visualizer.#createElement("div", [direction]);
+                current.appendChild(div);
+                current = div;
+            }
+            return current;
         }
     }
-    window.Visualizer = Visualizer;
+    // Main logic
+    const divs = $$("div.rtl-visualized");
+    let focused = null;
+    function onDblClick(e) {
+        e.preventDefault();
+        document.getSelection().removeAllRanges();
+        this.toggleAttribute("data-focused");
+        if (!this.hasAttribute("data-focused")) {
+            focused = null;
+            return;
+        }
+        if (focused) {
+            focused.removeAttribute("data-focused");
+        }
+        focused = this;
+    }
+    for (const div of divs) {
+        const text = div.textContent;
+        const visualizer = new Visualizer({ visualized: div });
+        visualizer.setText(text);
+        div.visualizer = visualizer; // DEBUG
+        if (div.hasAttribute("data-editable")) {
+            const input = document.createElement("input");
+            input.value = text;
+            input.addEventListener("input", () => {
+                visualizer.setText(input.value);
+            });
+            const rloBtn = document.createElement("button");
+            rloBtn.textContent = "⬅️";
+            rloBtn.title = "Insert RLO (or Ctrl+ArrowLeft)";
+            rloBtn.addEventListener("click", () => {
+                visualizer.insert(Visualizer.rlo);
+                input.value = visualizer.text;
+            });
+            const lroBtn = document.createElement("button");
+            lroBtn.textContent = "➡️";
+            lroBtn.title = "Insert LRO (or Ctrl+ArrowRight)";
+            lroBtn.addEventListener("click", () => {
+                visualizer.insert(Visualizer.lro);
+                input.value = visualizer.text;
+            });
+            const control = document.createElement("div");
+            control.appendChild(input);
+            control.appendChild(rloBtn);
+            control.appendChild(lroBtn);
+            control.classList.add("rtl-control");
+            visualizer.control = control;
+            div.after(control);
+            div.addEventListener("dblclick", onDblClick);
+        }
+    }
+    function onClick(e) {
+        if (focused && !focused.contains(e.target)) {
+            focused.removeAttribute("data-focused");
+            focused = null;
+        }
+    }
+    function onKeydown(e) {
+        if (!focused) {
+            return;
+        }
+        e.preventDefault();
+        const visualizer = focused.visualizer;
+        if (e.key === "Escape") {
+            focused.removeAttribute("data-focused");
+            focused = null;
+        } else if (e.key === "ArrowLeft") {
+            if (e.ctrlKey) {
+                visualizer.insert(Visualizer.rlo);
+            } else {
+                visualizer.move(-1);
+            }
+        } else if (e.key === "ArrowRight") {
+            if (e.ctrlKey) {
+                visualizer.insert(Visualizer.lro);
+            } else {
+                visualizer.move(1);
+            }
+        } else if (e.key === "Backspace") {
+            visualizer.backspace();
+        } else if (e.key === "Delete") {
+            visualizer.delete();
+        } else if (e.key.length === 1) {
+            visualizer.insert(e.key);
+        }
+        const input = visualizer.control.querySelector("input");
+        input.value = visualizer.text;
+    }
+    document.addEventListener("click", onClick);
+    document.addEventListener("keydown", onKeydown);
+    // Style settings
+    const checkboxes = $$("input[type=checkbox]");
+    const styles = $$("#markdown > style");
+    for (let i = 0; i < checkboxes.length; i++) {
+        const checkbox = checkboxes[i];
+        const style = styles[i + 1];
+        // Wrap the checkbox with a label
+        const label = document.createElement("label");
+        const li = checkbox.parentNode;
+        const children = [...li.childNodes];
+        for (const child of children) {
+            label.appendChild(child);
+        }
+        li.appendChild(label);
+        // Enable the checkbox
+        checkbox.addEventListener("change", () => {
+            style.disabled = !checkbox.checked;
+        });
+        checkbox.disabled = false;
+        style.disabled = !checkbox.checked;
+    }
+    // Remember to clean up
+    window.addEventListener("note_loading", () => {
+        document.removeEventListener("click", onClick);
+        document.removeEventListener("keydown", onKeydown);
+        // for (const div of divs) {
+        //     const visualizer = div.visualizer;
+        //     if (visualizer.control) {
+        //         const control = visualizer.control;
+        //         control.querySelector("input").removeEventListener("input", visualizer.setText);
+        //         div.removeEventListener("dblclick", onDblClick);
+        //     }
+        // }
+    }, { once: true });
 })();
--- notes/rtl_lab.md
+++ notes/rtl_lab.md
@@ -6,13 +6,117 @@ description: Some explanations and utils on Right-To-Left Override and Left-To-R
 scripts: true
 ---
 
+<style>
+    .rtl-visualized {
+        padding: 0.5em;
+        margin: 1em 0;
+        border: 1px dashed var(--border-dim);
+        border-radius: 0.5em;
+        transition: border-color 0.2s ease-in-out;
+
+        &:hover {
+            border-color: var(--border);
+        }
+
+        &[data-editable] {
+            .cursor::before {
+                content: "|";
+                color: var(--text);
+                animation: none;
+            }
+        }
+
+        &[data-focused] {
+            border-color: cyan;
+
+            .cursor::before {
+                animation: blink 1s infinite;
+            }
+        }
+
+        div {
+            padding: 0;
+            display: inline-block;
+            border: 1px solid;
+            margin: 2px;
+
+            &.rlo {
+                color: red;
+            }
+
+            &.lro {
+                color: green;
+            }
+        }
+    }
+
+    .rtl-control {
+        padding: 0.5em;
+        margin: 1em 0;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 1em;
+    }
+
+    ul > li > label {
+        display: inline-block;
+        width: 100%;
+    }
+
+    @keyframes blink {
+        0%, 50% {
+            opacity: 1;
+        }
+        51%, 100% {
+            opacity: 0;
+        }
+    }
+</style>
+<style>
+    /* Hide RLO and LRO characters */
+    .rtl-visualized {
+        span.rlo, span.lro {
+            display: none;
+        }
+    }
+</style>
+<style>
+    /* Hide border */
+    .rtl-visualized div {
+        border: none;
+        margin: 0;
+    }
+</style>
+
 # RTL Lab
 
 ## Explanation
 
 The Unicode characters `U+202E` and `U+202D` are used to change the text direction of a string. The `U+202E` character is used to change the text direction to RTL (Right-To-Left) and the `U+202D` character is used to change the text direction to LTR (Left-To-Right). Thus, the `U+202E` character is called `RLO` (Right-To-Left Override) and the `U+202D` character is called `LRO` (Left-To-Right Override).
 
+Below are some visualizations of the `RLO` and `LRO` characters. `RLO` is represented by the emoji `⬅️` and `LRO` is represented by the emoji `➡️`.
 
+| Direction | Color |
+| --------- | ----- |
+| Default   | unset |
+| LTR       | Green |
+| RTL       | Red   |
+
+<div class="rtl-visualized">Before LRO&#x202D;After LRO</div>
+<div class="rtl-visualized">Before RLO&#x202E;After RLO</div>
+<div class="rtl-visualized">Part A&#x202E;Part B&#x202D;Part C</div>
+
+## Playground
+
+Double click on the visualized text to focus on it, double click again or press `ESC` or click outside to unfocus. You can then use the arrow keys to move the cursor, `Backspace` and `Delete` keys to delete characters, and type to insert characters. Also, you can preview/edit the raw string on the input, and insert `RLO` and `LRO` characters by clicking the buttons.
+
+<div class="rtl-visualized" data-editable>[Prefix]&#x202E;[xiffuS]&#x202D;Anytext</div>
+
+## Settings
+
+- [ ] Hide `RLO` and `LRO` characters.
+- [ ] Hide border.
 
 ## References
 
