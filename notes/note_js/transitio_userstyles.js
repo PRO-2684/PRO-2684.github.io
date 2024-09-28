6f35c93c7e3199b5c63aa83504f66752c3e8bdc2
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2024-09-28 22:51:47 +0800

    Update `transitio_userstyles`

--- notes/note_js/transitio_userstyles.js
+++ notes/note_js/transitio_userstyles.js
@@ -3,7 +3,7 @@
     const styles = await (await fetch(url)).json();
     const templateContent = $("#userstyle-template").content;
     const container = $("#userstyles");
-    for (const style of styles) {
+    function addStyleCard(style) {
         const node = templateContent.cloneNode(true);
         const name = node.querySelector(".name");
         name.textContent = style.name;
@@ -24,4 +24,18 @@
         // install.href = `llqqnt://transitio/install-userstyle?url=${encodeURIComponent(style.download)}`;
         container.appendChild(node);
     }
+    styles.forEach(addStyleCard);
+    addStyleCard({
+        name: "Share Your UserStyle",
+        description: "分享您的用户样式",
+        author: "You",
+        link: "https://github.com/PRO-2684/transitio/issues/4",
+        download: "https://github.com/PRO-2684/transitio/wiki",
+        preprocessor: "any",
+    });
+    const share = container.lastElementChild;
+    share.querySelector(".author").href = "https://github.com/settings/profile";
+    const download = share.querySelector(".download");
+    download.textContent = "Wiki";
+    download.title = "Transitio Wiki";
 })();
